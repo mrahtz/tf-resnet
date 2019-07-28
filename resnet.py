a0516eeb821b973d0f4874fbde2e366864103d14
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-07-28 21:27:06 +0200

    Refactor

--- resnet.py
+++ resnet.py
@@ -1,19 +1,16 @@
 import re
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import GlobalAveragePooling2D, Dense, Conv2D
+from tensorflow.python.keras.layers import GlobalAveragePooling2D, Dense
 
 from layers import ConvBNRelu, ConvBNReluResidualBlock, ConvBNReluBlock
 
 
 def make_resnet(net):
-    match = re.match(r'([a-z]*)(\d+)', net)
-    net_type, n_layers = match.group(1), match.group(2)
-    residual = (net_type == 'resnet')
-    n = (int(n_layers) - 2) // 6
+    n, residual = parse_net(net)
     print("Net: detected n = {} {} shortcuts".format(n, 'with' if residual else 'without'))
 
     Block = ConvBNReluResidualBlock if residual else ConvBNReluBlock
-    _layers = [
+    layers = [
         ConvBNRelu(strides=1, filters=16, input_shape=(32, 32, 3)),
 
         Block(strides=1, filters=16),
@@ -28,7 +25,14 @@ def make_resnet(net):
         GlobalAveragePooling2D(),
         Dense(10, 'softmax')
     ]
+    layers = flatten_layers(layers)
 
+    model = Sequential(layers)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+    return model
+
+
+def flatten_layers(_layers):
     layers = []
     for x in _layers:
         try:
@@ -36,7 +40,12 @@ def make_resnet(net):
                 layers.append(l)
         except TypeError:
             layers.append(x)
+    return layers
 
-    model = Sequential(layers)
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
-    return model
+
+def parse_net(net):
+    match = re.match(r'([a-z]*)(\d+)', net)
+    net_type, n_layers = match.group(1), match.group(2)
+    residual = (net_type == 'resnet')
+    n = (int(n_layers) - 2) // 6
+    return n, residual
